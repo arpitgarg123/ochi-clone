cdc9d300d7282ab2c2ec8f593a163e23264c8f82
arpitgarg123 <arpitgarg424@gmail.com>
2024-12-18 14:58:28 +0530

    added images

--- src/components/Feature.jsx
+++ src/components/Feature.jsx
@@ -10,16 +10,16 @@ function Feature() {
       <h1 className='ml-12 text-[3.7vw] font-thin'>Featured projects</h1>
       <div className='w-full border-t-[1px] border-t-zinc-700 mt-10'>
        <WrapperCard>
-       <Card heading={"Cardboard Spaceship"} imgSrc={"https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-663x551.png"} buttonList ={["branded template" , "sales deck", "social media templates"]} hoverText={"cardboard spaceship"} />
-       <Card heading={"AH2 & Matt Horn"} imgSrc={"https://ochi.design/wp-content/uploads/2024/08/Frame-481692-1-663x551.png"} buttonList ={["pitch deck"]} hoverText={"AH2 & Matt Horn"} />
+       <Card heading={"Cardboard Spaceship"} imgSrc={"images/img1.png"} buttonList ={["branded template" , "sales deck", "social media templates"]} hoverText={"cardboard spaceship"} />
+       <Card heading={"AH2 & Matt Horn"} imgSrc={"images/img2.png"} buttonList ={["pitch deck"]} hoverText={"AH2 & Matt Horn"} />
        </WrapperCard>
        <WrapperCard>
-       <Card heading={"Cardboard Spaceship"} imgSrc={"https://ochi.design/wp-content/uploads/2023/10/Fyde_Illustration_Crypto_2-663x551.png"} buttonList ={["audit","copywriting","sales deck","slides design"]} hoverText={"fyde"} />
-       <Card heading={"AH2 & Matt Horn"} imgSrc={"https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg"} buttonList ={["agency","company presentation"]} hoverText={"vise"} />
+       <Card heading={"Cardboard Spaceship"} imgSrc={"images/img3.png"} buttonList ={["audit","copywriting","sales deck","slides design"]} hoverText={"fyde"} />
+       <Card heading={"AH2 & Matt Horn"} imgSrc={"images/img4.jpg"} buttonList ={["agency","company presentation"]} hoverText={"vise"} />
        </WrapperCard>
        <WrapperCard>
-       <Card heading={"Cardboard Spaceship"} imgSrc={"https://ochi.design/wp-content/uploads/2023/08/Frame-3875-663x551.jpg"} buttonList ={["brand identity","design research","investor deck"]} hoverText={"trawa"} />
-       <Card heading={"AH2 & Matt Horn"} imgSrc={"https://ochi.design/wp-content/uploads/2022/12/PB-Front-4-663x551.png"} buttonList ={["branded template"]} hoverText={"premium blend"} />
+       <Card heading={"Cardboard Spaceship"} imgSrc={"images/img5.jpg"} buttonList ={["brand identity","design research","investor deck"]} hoverText={"trawa"} />
+       <Card heading={"AH2 & Matt Horn"} imgSrc={"images/img6.png"} buttonList ={["branded template"]} hoverText={"premium blend"} />
        </WrapperCard>
       </div>
       <div className='w-full  flex justify-center mt-20'>
